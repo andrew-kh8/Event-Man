ba40c555a0c294e159bc790b64bb3c9224bfeea9
andrew <horolskyandrey@gmail.com>
2025-04-03 00:19:51 +0300

    organizations index page

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem "thruster", require: false
 gem "activerecord-postgis-adapter"
 gem "rgeo"
 
+gem "pagy"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_02_194309) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_02_195712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "postgis"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "activity_field"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
     t.string "auth_name", limit: 256
