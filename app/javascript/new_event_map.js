fbc420fcb286f93292a439f88dba0bea6310e53e
andrew <horolskyandrey@gmail.com>
2025-04-09 23:12:18 +0300

    upd assets and script using

--- app/javascript/new_event_map.js
+++ app/javascript/new_event_map.js
@@ -10,22 +10,34 @@ $(document).ready(function () {
   map = L.map("map", {
     center: [59, 87],
     zoom: zoom,
+    attributionControl: false,
   });
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 23,
   }).addTo(map);
 
-  var wfs_hunter_layer = L.Geoserver.wfs("http://0.0.0.0:8080/geoserver/wfs", {
+  L.Control.geocoder().addTo(map);
+
+  var event_layer = L.Geoserver.wfs("http://0.0.0.0:8080/geoserver/wfs", {
     layers: "event_man:events",
   });
 
   map.on("click", (event) => {
-    $("#event_location").val([event.latlng.lat, event.latlng.lng].join(" "));
-    console.log([event.latlng.lat, event.latlng.lng].join(" "));
+    $("#event_location").val([event.latlng.lng, event.latlng.lat].join(" "));
+
+    fetch(
+      `https://api.geoapify.com/v1/geocode/reverse?lat=${event.latlng.lat}&lon=${event.latlng.lng}&lang=ru&apiKey=3cbba0dac3564cb590445ae60cc44eea`,
+      { method: "GET" }
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        $("#event_location_name").val(result.features[0].properties.formatted)
+      )
+      .catch((error) => console.log("error", error));
+
+    marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map);
   });
 
-  wfs_hunter_layer.addTo(map);
+  event_layer.addTo(map);
 });
