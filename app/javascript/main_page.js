09775cb8c5dd40528f26792781fff74d9aa77115
andrew <horolskyandrey@gmail.com>
2025-04-09 23:11:26 +0300

    add link to event in main page map

--- app/javascript/main_page.js
+++ app/javascript/main_page.js
@@ -8,20 +8,29 @@ $(document).ready(function () {
   map = L.map("map", {
     center: [coord.lat, coord.lng],
     zoom: zoom,
+    attributionControl: false,
   });
 
   event_points = L.Geoserver.wfs("http://0.0.0.0:8080/geoserver/wfs", {
     layers: "event_man:events",
     onEachFeature: function (feature, layer) {
-      console.log(1);
+
+      layer.bindPopup(
+        `<a href='/organizations/${feature.properties.organization_id}/events/${feature.id.split('.')[1]}' data-turbo='false'>Посмотреть мероприятие</a>`
+      );
+
+      layer
+        .bindTooltip(
+          feature.properties.name,
+          { permanent: false }
+        )
+        .openTooltip();
     },
   });
 
   event_points.addTo(map);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 23,
   }).addTo(map);
 });
