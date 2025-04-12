b8dd710ad3c8156a56576428ddd5a4f6be33fa8b
andrew <horolskyandrey@gmail.com>
2025-04-12 20:39:34 +0300

    upd event search

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,7 +1,15 @@
 class MainPageController < ApplicationController
   include Pagy::Backend
 
-  def index; end
+  def index
+    filtered_events = if params[:event_search].present?
+                        Event.where('name ILIKE ?', ['%', params[:event_search][:event_name], '%'].join)
+                      else
+                        Event.all
+                      end
+
+    @pagy, @events = pagy(filtered_events)
+  end
 
   def event_list
     @pagy, @events = pagy(Event.all)
