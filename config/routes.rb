7833781104ea017da57d2c3581ddd635de038a26
andrew <horolskyandrey@gmail.com>
2025-04-20 13:05:25 +0300

    install avo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  authenticate :person, -> person { person.email == ENV['ADMIN_EMAIL'] } do
+    mount_avo at: '/avo'
+  end
+
   root "main_page#index"
   
   get "event_list" => "main_page#event_list"
