374a84b789b11a2014e0844b9e6a863276970989
andrew <horolskyandrey@gmail.com>
2025-04-20 16:19:45 +0300

    localization

--- app/javascript/new_event_map.js
+++ app/javascript/new_event_map.js
@@ -1,42 +1,5 @@
 $(document).ready(function () {
-  console.log("new EVENT 1");
-
-  var zoom = 4;
-  var coord = {
-    lat: 59,
-    lng: 87,
-  };
-
-  var marker = L.marker([coord.lat, coord.lng]);
-
-  map = L.map("map", {
-    center: [59, 87],
-    zoom: zoom,
-    attributionControl: false,
-  });
-
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 23,
-  }).addTo(map);
-
-  map.on("click", (event) => {
-    $("#event_location").val([event.latlng.lng, event.latlng.lat].join(" "));
-
-    fetch(
-      `https://api.geoapify.com/v1/geocode/reverse?lat=${event.latlng.lat}&lon=${event.latlng.lng}&lang=ru&apiKey=3cbba0dac3564cb590445ae60cc44eea`,
-      { method: "GET" }
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        $("#event_location_name").val(result.features[0].properties.formatted)
-      )
-      .catch((error) => console.log("error", error));
-
-    newLatLng = new L.LatLng(event.latlng.lat, event.latlng.lng);
-    marker.setLatLng(newLatLng);
-  });
-
-  marker.addTo(map);
+  console.log("new EVENT");
 
   // date time picker
 
@@ -74,4 +37,41 @@ $(document).ready(function () {
       firstDay: 1,
     },
   });
+
+  var zoom = 4;
+  var coord = {
+    lat: 59,
+    lng: 87,
+  };
+
+  var marker = L.marker([coord.lat, coord.lng]);
+
+  map = L.map("map", {
+    center: [59, 87],
+    zoom: zoom,
+    attributionControl: false,
+  });
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 23,
+  }).addTo(map);
+
+  map.on("click", (event) => {
+    $("#event_location").val([event.latlng.lng, event.latlng.lat].join(" "));
+
+    fetch(
+      `https://api.geoapify.com/v1/geocode/reverse?lat=${event.latlng.lat}&lon=${event.latlng.lng}&lang=ru&apiKey=3cbba0dac3564cb590445ae60cc44eea`,
+      { method: "GET" }
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        $("#event_location_name").val(result.features[0].properties.formatted)
+      )
+      .catch((error) => console.log("error", error));
+
+    newLatLng = new L.LatLng(event.latlng.lat, event.latlng.lng);
+    marker.setLatLng(newLatLng);
+  });
+
+  marker.addTo(map);
 });
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,6 @@ class Notification < ApplicationRecord
   belongs_to :person
   belongs_to :author, polymorphic: true
   belongs_to :target, polymorphic: true
+
+  scope :unread, -> { where(read: false) }
 end
