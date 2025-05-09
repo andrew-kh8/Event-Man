66d8d94681726a184cb39510cc5a056ff46d9b5f
andrew <horolskyandrey@gmail.com>
2025-05-09 22:22:43 +0300

    upd orgs system specs

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :organization do
     name { FFaker::Company.name }
     description { FFaker::Lorem.paragraph }
+    activity_field { FFaker::Skill.specialty }
+
     email { FFaker::Internet.email }
     password { FFaker::Internet.password }
   end
--- spec/system/organizations/index_spec.rb
+++ spec/system/organizations/index_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'index organizations', type: :system do
-  let!(:first_organization) { create(:organization) }
-  let!(:second_organization) { create(:organization) }
-  let!(:third_organization) { create(:organization) }
+  let!(:first_organization) { create(:organization, name: 'First name (1)') }
+  let!(:second_organization) { create(:organization, name: 'Second name second') }
+  let!(:third_organization) { create(:organization, name: 'C.K. & Co.') }
 
   it 'shows all organizations' do
     visit organizations_path
@@ -15,5 +15,25 @@ RSpec.describe 'index organizations', type: :system do
     expect(page).to have_link(href: organization_path(first_organization.id))
       .and have_link(href: organization_path(second_organization.id))
       .and have_link(href: organization_path(third_organization.id))
+  end
+
+  context 'when find by name' do
+    it 'shows organizations by name' do
+      visit organizations_path
+
+      find_by_id('organization_name').fill_in(with: 'name')
+      click_button('Поиск')
+
+      expect(page).to have_content(first_organization.name)
+        .and have_content(second_organization.name)
+        .and have_no_content(third_organization.name)
+
+      find_by_id('organization_name').fill_in(with: 'C.K')
+      click_button('Поиск')
+
+      expect(page).to have_content(third_organization.name)
+        .and have_no_content(first_organization.name)
+        .and have_no_content(second_organization.name)
+    end
   end
 end
