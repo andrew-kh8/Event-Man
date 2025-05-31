505dc5adddda72e47da64cbd1c968e366f965dbf
andrew <horolskyandrey@gmail.com>
2025-05-31 17:58:22 +0300

    organizations controller & views

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,84 +1,62 @@
 class OrganizationsController < ApplicationController
   include Pagy::Backend
 
-  before_action :set_organization, only: %i[show edit update destroy]
+  before_action :set_organization, only: %i[edit update destroy]
   before_action :authenticate_organization!, only: %i[edit update destroy]
 
-  # GET /organizations
   def index
-    organizations = if params[:organization_name]
-                      Organization.where('name ILIKE ?', "%#{params[:organization_name]}%")
-                    else
-                      Organization.all
-                    end
-
-    if params[:favorite] == 'true'
-      fav_ids = []
-
-      if cookies[:starred_organizations].present?
-        fav_ids += cookies[:starred_organizations].gsub(/[0-9]*/).compact_blank
-      end
-
-      if person_signed_in?
-        fav_ids += current_person.starred_organizations.select(:organization_id).pluck(:organization_id)
-      end
-
-      organizations = organizations.where(id: fav_ids)
-    end
+    organizations = find_organization
+    organizations = select_favorite_organizations(organizations) if params[:favorite] == 'true'
 
     @pagy, @organizations = pagy(organizations)
   end
 
-  # GET /organizations/1
   def show
     @organization = Organization.includes(:events).find(params[:id])
   end
 
-  # GET /organizations/new
-  def new
-    @organization = Organization.new
-  end
+  def edit; end
 
-  # GET /organizations/1/edit
-  def edit
-    @organization = Organization.find(params[:id])
-  end
-
-  # POST /organizations
-  def create
-    @organization = Organization.new(organization_params)
-
-    if @organization.save
-      redirect_to @organization, notice: 'Organization was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /organizations/1
   def update
     if @organization.update(organization_params)
-      redirect_to @organization, notice: 'Organization was successfully updated.', status: :see_other
+      redirect_to @organization, notice: 'Организация успешно обновлена', status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /organizations/1
   def destroy
     @organization.destroy!
-    redirect_to organizations_path, notice: 'Organization was successfully destroyed.', status: :see_other
+    redirect_to organizations_path, notice: 'Организация была полностью удалена', status: :see_other
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_organization
     @organization = Organization.find(params.expect(:id))
   end
 
-  # Only allow a list of trusted parameters through.
   def organization_params
     params.expect(organization: %i[name description activity_field logo])
+  end
+
+  def find_organization
+    if params[:organization_name]
+      Organization.where('name ILIKE ?', "%#{params[:organization_name]}%")
+    else
+      Organization.all
+    end
+  end
+
+  def select_favorite_organizations(organizations)
+    fav_ids = []
+
+    fav_ids += cookies[:starred_organizations].gsub(/[0-9]*/).compact_blank if cookies[:starred_organizations].present?
+
+    if person_signed_in?
+      fav_ids += current_person.starred_organizations.select(:organization_id).pluck(:organization_id)
+    end
+
+    organizations.where(id: fav_ids)
   end
 end
